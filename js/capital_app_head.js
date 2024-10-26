6f24099e8cf97c3950609853e863ea7a9cce93af
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-10-26 13:40:43 +0300

    first commit

---
+++ js/capital_app_head.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["capital_app_head"],{"4oai":function(a,n,i){"use strict";i.r(n);var o=i("EVdn"),p=i.n(o);window.$=p.a}},[["4oai","runtime",1]]]);
