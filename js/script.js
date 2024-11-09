6394e860d912934a2edd1ca0e44eb676af0f8fd8
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-11-09 10:19:47 +0300

    Fix mobile version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-const openSelect = () => {
-  const multiselectBlocks = document.querySelectorAll(".multiselect");
+const toggleFilter = () => {
+  const rtToggle = document.querySelector(".rt-toggle-collapse");
+  const rtCollapse = document.querySelector(".rt-collapse");
 
-  multiselectBlocks.forEach((parent) => {
-    parent.addEventListener("click", function (event) {
-      event.stopPropagation();
-      parent.classList.toggle("open");
+  if (rtToggle) {
+    rtToggle.addEventListener("click", () => {
+      rtToggle.classList.toggle("is-opened");
+      rtCollapse.classList.toggle("is-opened");
     });
-  });
+  }
 };
+toggleFilter();
 
-openSelect();
+const toggleMobileFilter = () => {
+  const rtToggleBtn = document.querySelector(".js-catalog-btn");
+  const rtCatalogBtn = document.querySelector(".rt-catalog-filter_btn");
+  const rtCalendarBtn = document.querySelector(".rt-calendar-filter_btn");
+
+  if (rtToggleBtn) {
+    rtToggleBtn.addEventListener("click", () => {
+      rtToggleBtn.nextElementSibling.classList.toggle("active");
+    });
+  }
+
+  if (rtCatalogBtn) {
+    rtCatalogBtn.addEventListener("click", () => {
+      rtCatalogBtn.nextElementSibling.classList.toggle("active");
+    });
+  }
+
+  if (rtCalendarBtn) {
+    rtCalendarBtn.addEventListener("click", () => {
+      rtCalendarBtn.nextElementSibling.classList.toggle("active");
+    });
+  }
+};
+toggleMobileFilter();
